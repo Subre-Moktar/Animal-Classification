45412d859e1edf262e79795b6a13e42e2aebc292
Subre Moktar <109233694+Subre-Moktar@users.noreply.github.com>
2024-05-01 20:33:14 -0500

    Add files via upload

--- detect.py
+++ detect.py
@@ -153,7 +153,7 @@ def detect(save_img=False):
                         else:  # stream
                             fps, w, h = 30, im0.shape[1], im0.shape[0]
                             save_path += '.mp4'
-                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'avc1'), fps, (w, h))
                     vid_writer.write(im0)
 
     if save_txt or save_img:
